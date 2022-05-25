c7599faa55b02360ec36ccd4038a387aad822b85
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-25 17:40:13 -0300

    feat: :memo: add model and migration categories

---
+++ src/database/migrations/20220525203539-Categories.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, 
+        autoIncrement: true,
+      },
+      name: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};
