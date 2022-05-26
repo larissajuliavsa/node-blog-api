c1eb6f545d3fbe3c7a775ac8c2850fc0f4bf5e5e
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-26 19:03:49 -0300

    feat: :memo: add user services

--- src/database/services/index.js
+++ src/database/services/index.js
@@ -1,5 +1,7 @@
 const login = require('./login.services');
+const user = require('./user.services');
 
 module.exports = {
   login,
+  user,
 };
---
+++ src/database/services/user.services.js
@@ -0,0 +1,30 @@
+const { User } = require('../models');
+const { generateJWT } = require('../middleware/tokenMiddleware');
+
+const errorMessage = (status, message) => ({
+  status,
+  message,
+});
+
+const getUser = async ({ displayName, email, password, image }) => {
+  const data = await User.findOne({ where: { email, password } });
+
+  if (data) throw errorMessage(409, 'User already registered');
+
+  const create = User.create({ displayName, email, password, image });
+
+  const payload = {
+    id: create.id,
+    displayName: create.displayName,
+    email: create.email,
+    image: create.image,
+  };
+
+  const user = generateJWT({ payload });
+
+  return user;
+};
+
+module.exports = {
+  getUser,
+};
