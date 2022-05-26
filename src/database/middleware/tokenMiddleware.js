2bba6535da9e72fd2356822d513030689c2e5a21
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-26 20:05:07 -0300

    feat: :memo: add verify tokens middleware

--- src/database/middleware/tokenMiddleware.js
+++ src/database/middleware/tokenMiddleware.js
@@ -6,12 +6,34 @@ const jwtConfig = {
   algorithm: 'HS256',
 };
 
+const errorMessage = (status, message) => ({
+  status,
+  message,
+});
+
 const generateJWT = (payload) => {
   const token = jwt.sign({ data: payload }, process.env.JWT_SECRET, jwtConfig);
 
   return token;
 };
 
+const validateJWT = (req, _res, next) => {
+  const { headers: { authorization } } = req;
+
+  if (!authorization) throw errorMessage(401, 'Token not found');  
+
+  try {
+    const verify = jwt.verify(authorization, process.env.JWT_SECRET);
+
+    if (!verify.data) throw errorMessage(401, 'Expired or invalid token');
+    
+    next();
+  } catch (err) {
+    throw errorMessage(401, 'Expired or invalid token');
+  }
+};
+
 module.exports = { 
   generateJWT,
+  validateJWT,
 };
