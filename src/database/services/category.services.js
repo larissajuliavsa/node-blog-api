ba184c94116c30aa45473bd4168ac3118c0eaa93
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-26 20:49:01 -0300

    feat: :memo: add get categories services

--- src/database/services/category.services.js
+++ src/database/services/category.services.js
@@ -1,19 +1,16 @@
 const { Category } = require('../models');
-// const { generateJWT } = require('../middleware/tokenMiddleware');
+
+const getCategories = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
 
 const createCategory = async (name) => {
   const create = Category.create({ name });
-
-  // const payload = {
-  //   id: create.id,
-  //   name: create.name,
-  // };
-
-  // const category = generateJWT({ payload });
-
   return create;
 };
 
 module.exports = {
+  getCategories,
   createCategory,
 };
