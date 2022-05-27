cb7d559044405a56caec4a62a5025db07e6f9508
Larissa Araujo <larissa.jvsa@gmail.com>
2022-05-26 22:00:01 -0300

    feat: :memo: add get posts services

--- src/database/services/index.js
+++ src/database/services/index.js
@@ -1,9 +1,11 @@
 const login = require('./login.services');
 const user = require('./user.services');
 const category = require('./category.services');
+const post = require('./post.services');
 
 module.exports = {
   login,
   user,
   category,
+  post,
 };
